c71e442ae1fd18d8ea12573cc8bf15d525b99eb8
FrankieHobbins <acepainkiller@gmail.com>
2017-09-13 16:46:32 +0100

    Add files via upload

---
+++ weight_tool_add_boxes_to_bones.py
@@ -0,0 +1,27 @@
+import bpy
+
+print ("---")
+newObjectList = []
+
+armature = bpy.context.selected_objects[0].data
+
+for pose_bone in bpy.context.selected_pose_bones:
+    #check to see if bone is deform bone
+    for bone in armature.bones:
+        if pose_bone.name == bone.name:
+            if bone.use_deform:
+                print (bone.name + "  is getting processed")
+
+                obj = pose_bone.id_data
+                matrix_final = obj.matrix_world * pose_bone.matrix            
+                pose_bone_size = (pose_bone.head - pose_bone.tail).length
+                
+                #add cube to bone with vert weight named after bone and set to 1
+                bpy.ops.mesh.primitive_cube_add()
+                cube = bpy.context.selected_objects[0]    
+                cube.name = "WEIGHT_PROXY_" + pose_bone.name
+                cube.matrix_world = matrix_final
+                cube.scale = cube.scale * pose_bone_size * 0.3
+                bpy.ops.transform.translate(value= (0,pose_bone_size*0.5,0), constraint_axis=(False, True, False), constraint_orientation='LOCAL')
+                vg = cube.vertex_groups.new(pose_bone.name)
+                vg.add(range(0,len(cube.data.vertices)), 1, "ADD")
---
+++ weight_tool_get_weight_from_boxes.py
@@ -0,0 +1,46 @@
+import bpy
+
+print ("---")
+
+selObj = bpy.context.selected_objects[0]
+
+bpy.ops.object.modifier_remove(modifier="DELETE_ME_WP")
+bpy.data.objects.remove(bpy.data.objects["DELETE_ME_WP"])
+
+#remove all vertex groups
+for group in selObj.vertex_groups:
+    selObj.vertex_groups.remove(group)
+
+#join all bone boxes into one object called "DELETE_ME_WP"
+for obj in bpy.data.objects:
+    obj.select = False
+    if "WEIGHT_PROXY_" in obj.name:
+        obj.select = True
+        bpy.context.scene.objects.active = obj
+        
+bpy.ops.object.duplicate_move()
+bpy.ops.object.join()
+bpy.context.selected_objects[0].name = "DELETE_ME_WP"
+
+bpy.ops.object.select_all(action='TOGGLE')
+
+bpy.context.scene.objects.active = selObj
+selObj.select = True
+
+#apply data transfer to selOBJ from "DELETE_ME_WP"
+bpy.ops.object.modifier_add(type='DATA_TRANSFER')
+selObj.modifiers[len(selObj.modifiers)-1].name = "DELETE_ME_WP"
+
+while selObj.modifiers[0].name != "DELETE_ME_WP":
+    bpy.ops.object.modifier_move_up(modifier="DELETE_ME_WP")
+
+selObj.modifiers["DELETE_ME_WP"].use_vert_data = True
+selObj.modifiers["DELETE_ME_WP"].data_types_verts = {'VGROUP_WEIGHTS'}
+selObj.modifiers["DELETE_ME_WP"].vert_mapping = 'POLY_NEAREST'
+selObj.modifiers["DELETE_ME_WP"].object = bpy.data.objects["DELETE_ME_WP"]
+
+bpy.ops.object.datalayout_transfer(modifier="DELETE_ME_WP")
+
+#enable use_paint_mask_vertex 
+#select all verts bpy.ops.paint.vert_select_all(action='TOGGLE')
+#loop all vert groups and do bpy.ops.object.vertex_group_smooth()
