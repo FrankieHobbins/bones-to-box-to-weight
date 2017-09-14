32ed2db7cc8246701341d9d09cb9e68bcc4135af
FrankieHobbins <acepainkiller@gmail.com>
2017-09-14 12:57:21 +0100

    latest

--- weight_tool_add_boxes_to_bones.py
+++ weight_tool_add_boxes_to_bones.py
@@ -2,55 +2,60 @@ import bpy
 
 print ("---")
 
-newObjectList = []
+#if armature isn't selected object find first armature in the scene
+if bpy.context.selected_objects:
+    if bpy.context.selected_objects[0].type == 'ARMATURE':
+        armatureObj = bpy.context.selected_objects[0]
+        armature = bpy.context.selected_objects[0].data
 
-armatureObj = bpy.context.selected_objects[0]
-armature = bpy.context.selected_objects[0].data
+else:
+    for obj in bpy.data.objects:
+        if obj.type == 'ARMATURE':
+            armatureObj = obj
+            armature = obj.data
 
-#armature.pose_position = 'REST' dont need this if we parent box to bone
+#add boxes to pose bones only
+for pose_bone in armatureObj.pose.bones:
+    if armature.bones[pose_bone.name].use_deform:
+        obj = pose_bone.id_data
+        pose_bone_size = (pose_bone.head - pose_bone.tail).length
+    
+        #add cube to bone with vert weight named after bone and set to 1
+        bpy.ops.mesh.primitive_cube_add()
+        cube = bpy.context.selected_objects[0]
+        cube.name = "WEIGHT_PROXY_" + pose_bone.name
+        cube.draw_type = 'WIRE'
 
-for pose_bone in bpy.context.selected_pose_bones:
-    #check to see if bone is deform bone
-    for bone in armature.bones:
-        if pose_bone.name == bone.name:
-            if bone.use_deform:
-                obj = pose_bone.id_data
-                pose_bone_size = (pose_bone.head - pose_bone.tail).length
-            
-                #add cube to bone with vert weight named after bone and set to 1
-                bpy.ops.mesh.primitive_cube_add()
-                cube = bpy.context.selected_objects[0]    
-                cube.name = "WEIGHT_PROXY_" + pose_bone.name
-                cube.draw_type = 'WIRE'
+        #check to see if mesh data allready exists
+        preexistingmesh = False 
+        for mesh in bpy.data.meshes:
+            if mesh.name == "WEIGHT_PROXY_" + pose_bone.name:
+                preexistingmesh = True
+                cube.data = mesh
+        if preexistingmesh == False:
+            cube.data.name = cube.name
 
-                #check to see if mesh data allready exists                 
-                for mesh in bpy.data.meshes:
-                    if mesh.name == "WEIGHT_PROXY_" + pose_bone.name:
-                        cube.data = mesh
-                    else:
-                        cube.data.name = cube.name
-
-                #setup cube weight proxy
-                cube.data.use_fake_user = True #fake user so mesh data persists after WEIGHT_PROXY_ objects have been deleted
-                cube.scale = cube.scale * pose_bone_size * 0.3
-                cube.parent = armatureObj
-                cube.parent_type = 'BONE'
-                cube.parent_bone = pose_bone.name
-                cube.location = (0,-pose_bone_size*0.5,0)
-                
-                #add vertex group
-                vg = cube.vertex_groups.new(pose_bone.name)
-                vg.add(range(0,len(cube.data.vertices)), 1, "ADD")
-                
-                #lock transforms so that the object cant be changed in a way that isn't saved in the mesh data
-                bpy.context.object.lock_location[0] = True
-                bpy.context.object.lock_location[1] = True
-                bpy.context.object.lock_location[2] = True
-                
-                bpy.context.object.lock_rotation[0] = True
-                bpy.context.object.lock_rotation[1] = True
-                bpy.context.object.lock_rotation[2] = True
-                
-                bpy.context.object.lock_scale[0] = True
-                bpy.context.object.lock_scale[1] = True
-                bpy.context.object.lock_scale[2] = True
+        #setup cube weight proxy
+        cube.data.use_fake_user = True #fake user so mesh data persists after WEIGHT_PROXY_ objects have been deleted
+        cube.scale = cube.scale * pose_bone_size * 0.3
+        cube.parent = armatureObj
+        cube.parent_type = 'BONE'
+        cube.parent_bone = pose_bone.name
+        cube.location = (0,-pose_bone_size*0.5,0)
+        
+        #add vertex group
+        vg = cube.vertex_groups.new(pose_bone.name)
+        vg.add(range(0,len(cube.data.vertices)), 1, "ADD")
+        
+        #lock transforms so that the object cant be changed in a way that isn't saved in the mesh data
+        bpy.context.object.lock_location[0] = True
+        bpy.context.object.lock_location[1] = True
+        bpy.context.object.lock_location[2] = True
+        
+        bpy.context.object.lock_rotation[0] = True
+        bpy.context.object.lock_rotation[1] = True
+        bpy.context.object.lock_rotation[2] = True
+        
+        bpy.context.object.lock_scale[0] = True
+        bpy.context.object.lock_scale[1] = True
+        bpy.context.object.lock_scale[2] = True
--- weight_tool_get_weight_from_boxes.py
+++ weight_tool_get_weight_from_boxes.py
@@ -1,79 +1,85 @@
 import bpy
 
-print ("---")
+print ("--START--")
 
-selObj = bpy.context.selected_objects[0]
+def getWeightFromBoxes(selObj):
+    if selObj.hide == False:
+        #sloppy way to add armature to object if not allready there
+        hasArmature = False
+        for mod in selObj.modifiers:
+            if mod.type == 'ARMATURE':
+                hasArmature = True
+                armature = mod.object
+                
+        if hasArmature == False:
+            bpy.ops.object.modifier_add(type='ARMATURE')
+            for obj in bpy.data.objects:
+                if obj.type == 'ARMATURE':
+                    selObj.modifiers['Armature'].object = obj
+                    armature = obj
+                    break
+
+        armature.data.pose_position = 'REST'
+
+        #remove all vertex groups
+        for group in selObj.vertex_groups:
+            selObj.vertex_groups.remove(group)
+
+        #join all bone boxes into one object called "DELETE_ME_WP"
+        for obj in bpy.data.objects:
+            obj.select = False
+            if "WEIGHT_PROXY_" in obj.name:
+                obj.select = True
+                bpy.context.scene.objects.active = obj
+                
+        bpy.ops.object.duplicate_move()
+        bpy.ops.object.join()
+        bpy.context.selected_objects[0].name = "DELETE_ME_WP"
+
+        bpy.ops.object.select_all(action='TOGGLE')
+
+        bpy.context.scene.objects.active = selObj
+        selObj.select = True
+
+        #apply data transfer to selOBJ from "DELETE_ME_WP"
+        bpy.ops.object.modifier_add(type='DATA_TRANSFER')
+        selObj.modifiers[len(selObj.modifiers)-1].name = "DELETE_ME_WP"
+
+        while selObj.modifiers[0].name != "DELETE_ME_WP":
+            bpy.ops.object.modifier_move_up(modifier="DELETE_ME_WP")
+
+        selObj.modifiers["DELETE_ME_WP"].use_vert_data = True
+        selObj.modifiers["DELETE_ME_WP"].data_types_verts = {'VGROUP_WEIGHTS'}
+        selObj.modifiers["DELETE_ME_WP"].vert_mapping = 'POLY_NEAREST'
+        selObj.modifiers["DELETE_ME_WP"].object = bpy.data.objects["DELETE_ME_WP"]
+
+        bpy.ops.object.datalayout_transfer(modifier="DELETE_ME_WP")
+
+        #enable use_paint_mask_vertex 
+        #select all verts bpy.ops.paint.vert_select_all(action='TOGGLE')
+        #loop all vert groups and do bpy.ops.object.vertex_group_smooth()
+
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="DELETE_ME_WP")
+        bpy.ops.object.modifier_remove(modifier="DELETE_ME_WP")
+        bpy.data.objects.remove(bpy.data.objects["DELETE_ME_WP"])
+
+        armature.data.pose_position = 'POSE'
+
+thereHasToBeABetterWay = False
+
+if bpy.context.selected_objects:
+    if bpy.context.selected_objects[0].modifiers:
+        for mod in bpy.context.selected_objects[0].modifiers:
+            if mod.type == 'ARMATURE':
+                getWeightFromBoxes(bpy.context.selected_objects[0])
+                thereHasToBeABetterWay = True
+
+if thereHasToBeABetterWay == False:
+    for obj in bpy.context.scene.objects:
+        if obj.modifiers:
+            for mod in obj.modifiers:
+                if mod.type == 'ARMATURE':
+                    print(obj)
+                    getWeightFromBoxes(obj)
 
 
-#sloppy way to add armature to object if not allready there
-hasArmature = False
-for mod in selObj.modifiers:
-    if mod.type == 'ARMATURE':
-        hasArmature = True
-        armature = mod.object
-        
-if hasArmature == False:
-    bpy.ops.object.modifier_add(type='ARMATURE')
-    for obj in bpy.data.objects:
-        if obj.type == 'ARMATURE':
-            selObj.modifiers['Armature'].object = obj
-            armature = obj
-            break
-
-armature.data.pose_position = 'REST'
-
-"""
-try:
-    bpy.ops.object.modifier_remove(modifier="DELETE_ME_WP")
-except:
-    print("--")
-
-try:
-    bpy.data.objects.remove(bpy.data.objects["DELETE_ME_WP"])
-except:
-    print("-")
-"""
-
-#remove all vertex groups
-for group in selObj.vertex_groups:
-    selObj.vertex_groups.remove(group)
-
-#join all bone boxes into one object called "DELETE_ME_WP"
-for obj in bpy.data.objects:
-    obj.select = False
-    if "WEIGHT_PROXY_" in obj.name:
-        obj.select = True
-        bpy.context.scene.objects.active = obj
-        
-bpy.ops.object.duplicate_move()
-bpy.ops.object.join()
-bpy.context.selected_objects[0].name = "DELETE_ME_WP"
-
-bpy.ops.object.select_all(action='TOGGLE')
-
-bpy.context.scene.objects.active = selObj
-selObj.select = True
-
-#apply data transfer to selOBJ from "DELETE_ME_WP"
-bpy.ops.object.modifier_add(type='DATA_TRANSFER')
-selObj.modifiers[len(selObj.modifiers)-1].name = "DELETE_ME_WP"
-
-while selObj.modifiers[0].name != "DELETE_ME_WP":
-    bpy.ops.object.modifier_move_up(modifier="DELETE_ME_WP")
-
-selObj.modifiers["DELETE_ME_WP"].use_vert_data = True
-selObj.modifiers["DELETE_ME_WP"].data_types_verts = {'VGROUP_WEIGHTS'}
-selObj.modifiers["DELETE_ME_WP"].vert_mapping = 'POLY_NEAREST'
-selObj.modifiers["DELETE_ME_WP"].object = bpy.data.objects["DELETE_ME_WP"]
-
-bpy.ops.object.datalayout_transfer(modifier="DELETE_ME_WP")
-
-#enable use_paint_mask_vertex 
-#select all verts bpy.ops.paint.vert_select_all(action='TOGGLE')
-#loop all vert groups and do bpy.ops.object.vertex_group_smooth()
-
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="DELETE_ME_WP")
-bpy.ops.object.modifier_remove(modifier="DELETE_ME_WP")
-bpy.data.objects.remove(bpy.data.objects["DELETE_ME_WP"])
-
-armature.data.pose_position = 'POSE'
